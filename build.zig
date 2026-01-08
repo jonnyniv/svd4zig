3a482888b0223cbecfb65b60a43b127c5572db7c
Jonathan Niven <3747270+jonnyniv@users.noreply.github.com>
2026-01-08 12:50:34 +0000

    Adds check step and run step

--- build.zig
+++ build.zig
@@ -4,11 +4,47 @@ const builtin = @import("builtin");
 
 pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
-    const exe = b.addExecutable(.{
-        .name = "svd4zig",
+    const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
     });
+
+    const exe = b.addExecutable(.{
+        .name = "svd4zig",
+        .root_module = exe_mod,
+    });
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // This creates another `std.Build.Step.Compile`, but this one builds an executable
+    // rather than a static library.
+    const exe_check = b.addExecutable(.{
+        .name =  "svd4zig",
+        .root_module = exe_mod,
+    });
+    const check_step = b.step("check", "Just compile");
+    check_step.dependOn(&exe_check.step);
 
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
